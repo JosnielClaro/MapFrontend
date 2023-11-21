e653fb4069374adc978ad819677578654da9f222
Josniel <clarojosniel@gmail.com>
2023-11-21 04:17:36 -0500

    actualizacion

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -10,6 +10,8 @@
     <button @click="status3T2">status3 T2</button>
     <input type="text" v-model="input">
     <button @click="actDescT1">actDescT1</button>
+    <input type="number" v-model="orientinput">
+    <button @click="actOrientT3">actOrientT3</button>
   </div>
   <div class="map-container">
     <div id="cesiumContainer">
@@ -18,6 +20,8 @@
 </template>
 <script>
 
+import CesiumNavigation from "cesium-navigation-es6";
+
   export default {
     data(){
       return {
@@ -25,20 +29,24 @@
           latitude: 46.72,
           active: true,
           status: 1,
-          description: 'Turbina 1'},
+          description: 'Turbina 1',
+          orientation: 0},
         {longitude: 8.25,
           latitude: 46.87,
           active: false,
           status: 3,
-          description: 'Turbina 2'},
+          description: 'Turbina 2',
+          orientation: 90},
         {longitude: 8.38,
           latitude: 46.71,
           active: true,
           status: 2,
-          description: 'Turbina 3'}],
+          description: 'Turbina 3',
+          orientation: 180}],
         terrainProvider: null,
         viewer: null,
         input: "",
+        orientinput: null,
       }
     },
     watch: {
@@ -50,7 +58,12 @@
           items.forEach((element, index) => {
             var marker = markers[index]
             marker.description = element.description;
-            
+            var position = Cesium.Cartographic.fromDegrees(element.longitude, element.latitude);
+            Cesium.sampleTerrainMostDetailed(this.terrainProvider, [position]).then(function(data) {
+              var updatePosition = Cesium.Cartesian3.fromRadians(data[0].longitude,
+                  data[0].latitude, data[0].height);
+              marker.orientation = Cesium.Transforms.headingPitchRollQuaternion(updatePosition, new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(element.orientation + 270, 0, 0)));
+            })
             if (!element.active) {
               marker.model.runAnimations = false;
             }
@@ -58,13 +71,13 @@
               marker.model.runAnimations = true;
             }
             if (element.status == 1) {
-              marker.model.color = Cesium.Color.GREEN.withAlpha(0.9);
+              marker.model.color = Cesium.Color.GREEN.withAlpha(1);
             }
             if (element.status == 2) {
-              marker.model.color = Cesium.Color.BLUE.withAlpha(0.9);
+              marker.model.color = Cesium.Color.BLUE.withAlpha(1);
             }
             if (element.status == 3) {
-              marker.model.color = Cesium.Color.RED.withAlpha(0.9);
+              marker.model.color = Cesium.Color.RED.withAlpha(1);
             }
           });
         }
@@ -75,6 +88,9 @@
     methods: {
       actDescT1() {
         this.dataMarkers[0].description = this.input
+      },
+      actOrientT3() {
+        this.dataMarkers[2].orientation = this.orientinput
       },
       activeT1() {
         this.dataMarkers[0].active = !this.dataMarkers[0].active;
@@ -99,6 +115,15 @@
       },
       status3T2() {
         this.dataMarkers[1].status = 3;
+      },
+      degreesToRadians(degrees) {
+        return degrees * Math.PI / 180;
+      },
+      createOrientation(angleDegrees) {
+        var heading = this.degreesToRadians(angleDegrees);
+        var pitch = 0;
+        var roll = 0;
+        return Cesium.Transforms.headingPitchRollQuaternion(position, new Cesium.HeadingPitchRoll(heading, pitch, roll));
       },
       createMap() {
         const key = 'K1QdGd1zXrTbjToDlDPZ';
@@ -137,18 +162,26 @@
         this.viewer.scene.globe.depthTestAgainstTerrain = true;
         this.viewer.clock.multiplier = 1; // Esto hace que el tiempo avance a velocidad normal
         this.viewer.clock.shouldAnimate = true; // Esto asegura que el reloj esté activo
+        
+        let cesiumNavigation = new CesiumNavigation(this.viewer, {
+          enableCompass: true,
+          enableZoomControls: true,
+          enableDistanceLegend: true,
+          enableCompassOuterRing: true
+        });
       },
       addMarkers() {
         var viewer = this.viewer
         this.dataMarkers.forEach(marker => {
           //Obtener la altura del terreno
-          var updatePosition
           var position = Cesium.Cartographic.fromDegrees(marker.longitude, marker.latitude);
           Cesium.sampleTerrainMostDetailed(this.terrainProvider, [position]).then(function(data) {
+            var updatePosition = Cesium.Cartesian3.fromRadians(data[0].longitude,
+                data[0].latitude, data[0].height);
             var model = viewer.entities.add({
               name: '',
-              position: Cesium.Cartesian3.fromRadians(data[0].longitude,
-                data[0].latitude, data[0].height),
+              orientation: Cesium.Transforms.headingPitchRollQuaternion(updatePosition, new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(marker.orientation + 270, 0, 0))),
+              position: updatePosition,
               model: {
                 uri: "/EolicAnimate.glb",
                 scale: 300,
@@ -159,13 +192,13 @@
               model.model.runAnimations = false;
             }
             if (marker.status == 1) {
-              model.model.color = Cesium.Color.GREEN.withAlpha(0.9);
+              model.model.color = Cesium.Color.GREEN.withAlpha(1);
             }
             if (marker.status == 2) {
-              model.model.color = Cesium.Color.BLUE.withAlpha(0.9);
+              model.model.color = Cesium.Color.BLUE.withAlpha(1);
             }
             if (marker.status == 3) {
-              model.model.color = Cesium.Color.RED.withAlpha(0.9);
+              model.model.color = Cesium.Color.RED.withAlpha(1);
             }
           });
         })
